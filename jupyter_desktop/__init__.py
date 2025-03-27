9df04e5289dc1d689d72bd83860253d50e95e010
Gabrie50 <gcriste268@gmail.com>
2025-03-27 19:44:41 -0300

    Update __init__.py

--- jupyter_desktop/__init__.py
+++ jupyter_desktop/__init__.py
@@ -7,8 +7,8 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 
 def setup_desktop():
     # Detecta a resolução da tela automaticamente ou usa o padrão
-    width = os.getenv("SCREEN_WIDTH", "1300")
-    height = os.getenv("SCREEN_HEIGHT", "720")
+    width = os.getenv("SCREEN_WIDTH", "1100")
+    height = os.getenv("SCREEN_HEIGHT", "520")
     resolution = f"{width}x{height}"
 
     # Cria um diretório temporário para os sockets
